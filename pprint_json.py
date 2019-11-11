d4db926e44977faa835672a65bc5dc7003b0348e
SotM <sotm@mail.ru>
2019-11-11 20:12:08 +0300

    функции готовы

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,28 @@ import json
 
 
 def load_data(filepath):
-    pass
+    # fin = open(filepath, 'r', encoding='utf8')
+    fin = open(filepath, 'r', encoding='cp1251')
+    loaded_data = fin.read()
+    fin.close()
+
+    try:
+        data = json.loads(loaded_data, strict=False)
+    except ValueError as err:
+        print('Error:', err)
+        data = ''
+    except:
+        data = ''
+    else:
+        print('ok')
+
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    shops_data = load_data('shops.json')
+    pretty_print_json(shops_data)
